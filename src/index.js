53a32e7f2b35bcc0edce8bc5395686357c8a9372
Carsten Mielke <carsten.mielke@kartverket.no>
2017-02-26 22:45:42 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const pug = require('pug');
 // Compile the source code
-const compiledFunction = pug.compileFile('src/template.pug');
+const compiledTemplate = pug.compileFile('src/template.pug');
 
-// Lists all files in directorys
+// Lists all files in directorys (not used at the moment)
 const walkSync = (dir, filelist = []) => {
     fs.readdirSync(dir).forEach(file => {
         filelist = fs.statSync(path.join(dir, file)).isDirectory() ?
@@ -25,8 +25,9 @@ const directoryList = (dir, filelist = []) => {
     return filelist;
 };
 
+// write index.html
 const writeFile = (folders) => {
-    const html = compiledFunction({
+    const html = compiledTemplate({
         folders
     });
     fs.writeFile('index.html', html, (err) => {
@@ -36,6 +37,7 @@ const writeFile = (folders) => {
     });
 }
 
+// copy index.html to dir
 const copyFile = (dir, file = 'index.html') => {
     fs.createReadStream(file).pipe(fs.createWriteStream(path.join(dir, file)));
 }
